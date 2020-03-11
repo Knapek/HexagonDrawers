972a31051fb5c4169f7fd3c3a4948bf5a01a2d01
Knapek <knapekk@gmail.com>
2020-03-11 18:53:48 +0100

    Added attributes name, listOfItems. Added methods addDrawers, presentAllDrawers

---
+++ SetOfDrawers.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+class SetOfDrawers 
+{
+    private $name;
+    private $listOfDrawers;
+    
+    public function addDrawer():void
+    {
+
+    }
+    public function presentAllDrawers()
+    {
+        return $this->listOfDrawers;
+    }
+}
