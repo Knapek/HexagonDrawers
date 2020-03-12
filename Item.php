7a4db2f13e715a2a0d92ce398d87a7b4254dc4d8
Knapek <knapekk@gmail.com>
2020-03-12 11:28:46 +0100

    $name,$quantity, GetSetName(), GetSetQuantity()

--- Item.php
+++ Item.php
@@ -1,8 +1,30 @@
 <?php
 
 declare(strict_types=1);
-
+require_once('Drawer.php');
 class Item extends Drawer
 {
-    
+    private $name;
+    private $quantity;
+
+    public function getName():String
+    {
+        return $this->name;
+    }
+    public function setName(String $name):Item
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getQuantity():int
+    {
+        return $this->quantity;
+    }
+    public function setQuantity(int $quantity):Item
+    {
+        $this->quantity = $quantity;
+        return $this;
+    }
+
 }
