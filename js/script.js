8b8fe4744f00a6bdb706c92c9d07d66fc271a2bc
Knapek <knapekk@gmail.com>
2020-03-18 18:51:11 +0100

    script.js Hexagon Factory  working for one Hexagon

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var asd = 0;
+class Hexagon
+{
+    createName(){
+        asd++;
+        return `canvas${asd}`;
+    }
+    addHexagon() {
+        var canvas = document.querySelector('#canvas').getContext('2d'),
+        side = 0,
+        size = 100,
+        x = 100,
+        y = 100;
+
+        canvas.beginPath();
+        canvas.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));
+
+        for (side; side < 7; side++) {
+        canvas.lineTo(x + size * Math.cos(side * 2 * Math.PI / 6), y + size * Math.sin(side * 2 * Math.PI / 6));
+        }
+
+        canvas.fillStyle = "#333333";
+        canvas.fill();
+    } 
+}
+
+class HexagonFactory 
+{
+    static createHexagon() {
+        
+         return new Hexagon();      
+    }
+}
+
+var p = HexagonFactory.createHexagon();
+var a = HexagonFactory.createHexagon();
+var c = new Hexagon();
+console.log(a.createName());
+console.log(c.createName());
+console.log(p.createName());
+
+
